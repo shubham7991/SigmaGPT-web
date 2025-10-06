af36a30118277b6cae1c8fbe575cb2b6ff9ac144
shubham7991 <shubhamshukla2238@gmail.com>
2025-10-07 00:33:20 +0530

    Update API URL to production

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -23,7 +23,7 @@ function ChatWindow(){
             })
         };
         try{
-            const response=await fetch("http://localhost:8080/api/chat",options);
+            const response = await fetch("https://sigmagpt-web.onrender.com/api/chat", options);
             const res= await response.json();
             console.log(res);
             setReply(res.reply);
--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -8,7 +8,7 @@ function Sidebar(){
 
     const getAllThreads=async ()=>{
         try{
-            const response=await fetch("http://localhost:8080/api/thread");
+            const response=await fetch("https://sigmagpt-web.onrender.com/api/thread");
             const res= await response.json();
             const filteredData=res.map(thread=>({threadId:thread.threadId,title:thread.title}))
             // console.log(filteredData);
@@ -34,7 +34,7 @@ function Sidebar(){
     const changeThread=async(newThreadId)=>{
         setCurrThreadId(newThreadId);
         try{
-          const response= await fetch(`http://localhost:8080/api/thread/${newThreadId}`);
+          const response= await fetch(`https://sigmagpt-web.onrender.com/api/thread/${newThreadId}`);
           const res= await response.json();
           console.log(res);
           setPrevChats(res);
@@ -47,7 +47,7 @@ function Sidebar(){
 
     const deleteThread=async(threadId)=>{
         try{
-            const response= await fetch(`http://localhost:8080/api/thread/${threadId}`,{method:"DELETE"})
+            const response= await fetch(`https://sigmagpt-web.onrender.com/api/thread/${threadId}`,{method:"DELETE"})
             const res = await response.json();
             console.log(res);
 
